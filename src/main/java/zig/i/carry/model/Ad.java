3ed19bf6b3f6b935f865d3d9df5a74bac9316b5b
Kairat Doshekenov <kayrat@bk.ru>
2018-07-29 12:36:39 +0600

    change IDENTITY to SEQUENCE

--- src/main/java/zig/i/carry/model/Ad.java
+++ src/main/java/zig/i/carry/model/Ad.java
@@ -15,7 +15,7 @@ public abstract class Ad {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     protected Long id;
 
 //    @OneToMany
