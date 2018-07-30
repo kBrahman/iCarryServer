ad7610e6896fc1684051060006af359989cd4d6c
Kairat Doshekenov <kayrat@bk.ru>
2018-07-30 13:49:20 +0600

    bug fix

--- src/main/java/zig/i/carry/model/Ad.java
+++ src/main/java/zig/i/carry/model/Ad.java
@@ -84,7 +84,7 @@ public abstract class Ad {
 
     @Override
     public String toString() {
-        return "Ad{" +
+        return getClass().getSimpleName() + "{" +
                 "id=" + id +
                 ", countryFrom='" + countryFrom + '\'' +
                 ", countryTo='" + countryTo + '\'' +
