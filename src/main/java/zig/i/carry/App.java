cf809f034282fe539bdae72b0d1f4a3fc69b5283
Kairat Doshekenov <kayrat@bk.ru>
2018-07-30 14:11:49 +0600

    bug fix

--- src/main/java/zig/i/carry/App.java
+++ src/main/java/zig/i/carry/App.java
@@ -1,10 +1,12 @@
 package zig.i.carry;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {JacksonAutoConfiguration.class})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
