b1b0269be2eabc98a98ef235142aa4d0b5762f03
Kairat Doshekenov <kayrat@bk.ru>
2018-08-12 09:59:18 +0600

    bug fix

--- src/main/java/zig/i/carry/model/Contact.java
+++ src/main/java/zig/i/carry/model/Contact.java
@@ -1,12 +1,15 @@
 package zig.i.carry.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Contact {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String value;
