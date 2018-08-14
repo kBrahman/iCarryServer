939281fb0801423df71384db899589f9bb84c6ac
Kairat Doshekenov <kayrat@bk.ru>
2018-08-14 15:26:35 +0600

    add @DiscriminatorValue

--- src/main/java/zig/i/carry/model/OfferAd.java
+++ src/main/java/zig/i/carry/model/OfferAd.java
@@ -1,7 +1,9 @@
 package zig.i.carry.model;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("offer")
 public class OfferAd extends Ad {
 }
--- src/main/java/zig/i/carry/model/OrderAd.java
+++ src/main/java/zig/i/carry/model/OrderAd.java
@@ -1,7 +1,9 @@
 package zig.i.carry.model;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("order")
 public class OrderAd extends Ad {
 }
