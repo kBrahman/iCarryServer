132d928ff8fbd5ba43d70544c3352d0d2a29184d
Kairat Doshekenov <kayrat@bk.ru>
2018-08-15 05:07:57 +0600

    bug fix

--- src/main/java/zig/i/carry/controller/Controller.java
+++ src/main/java/zig/i/carry/controller/Controller.java
@@ -115,7 +115,7 @@ public class Controller {
     @RequestMapping(value = MY_ADS, method = RequestMethod.POST)
     private List<Ad> getMyAds(@RequestBody String login) {
         System.out.println("login=>" + login);
-        List<Ad> ads = adRepo.getAdsByUserLogin(login);
+        List<Ad> ads = adRepo.getAdsByUserLogin(login.substring(1, login.length() - 1));
         System.out.println(ads);
         return ads;
     }
